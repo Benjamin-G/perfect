bc16fe0acd24914a2952f16a20720e69164ed257
benjamin-g <brgeyer49@gmail.com>
2023-02-06 14:50:55 -0800

    feat: docs practice

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from tutorial import *
 
 
 @task(retries=3)
-def get_stars(repo: str, client):
+def get_stars(repo: str, client: httpx.Client) -> None:
+    """
+    Fetches all stars and prints them
+    """
     print(type(client))
     url = f"https://api.github.com/repos/{repo}"
     count = client.get(url).json()["stargazers_count"]
@@ -16,14 +19,22 @@ def get_stars(repo: str, client):
 
 
 @flow(name="GitHub Stars")
-def github_stars(repos: List[str]):
+def github_stars(repos: List[str]) -> None:
+    """
+    Gets the github stars for the list of repositories given using an httpx client
+    :param repos: list of repositories
+    """
     with httpx.Client() as client:
         for repo in repos:
             get_stars(repo, client)
 
 
 @task(cache_key_fn=task_input_hash, cache_expiration=timedelta(minutes=1))
-def hello_task(name_input):
+def hello_task(name_input: str) -> None:
+    """
+    prints a name to say hello to
+    :param name_input:
+    """
     # Doing some work
     print(f"Saying hello {name_input}")
     return "hello " + name_input
